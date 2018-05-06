8826ad704048264b317e0cb43698f3f6bb46c451
vaidas100 <vaidas100@gmail.com>
2018-05-06 15:25:07 +0300

    + logging

--- client_example.py
+++ client_example.py
@@ -1,13 +1,25 @@
 from Peekdata.DataGatewayAPI import ApiServices
 from Peekdata.DataGatewayAPI import RequestServices
 from Peekdata.DataGatewayAPI.Model import *
+import logging
 import time
 
 __author__ = 'Vaidotas Senkus'
 __email__ = 'vaidas100@gmail.com'
 
 
-print("Peekdata Data API Gateway examples\n")
+# logging configuration
+PRINT = 25
+logging.basicConfig(
+    level=PRINT,
+    format=format('%(message)s'),
+    handlers=[
+        logging.FileHandler(filename='client_example.log', mode='w'),
+        logging.StreamHandler(),
+    ]
+)
+
+logging.log(PRINT, "Peekdata Data API Gateway examples:\n")
 
 # initialize client
 api = ApiServices.ApiClient("demo.peekdata.io", 8080, "http")
@@ -17,7 +29,7 @@ t1 = time.time()
 health = api.healthCheck()
 t2 = time.time()
 if health:
-    print("Healthcheck ok ({time:.2f}ms)\n".format(
+    logging.log(PRINT, "Healthcheck ok ({time:.2f}ms).\n".format(
         time=(t2 - t1)*1000
     ))
 else:
@@ -26,7 +38,7 @@ else:
 
 # create request
 request = RequestServices.getTwoDimensionsTwoMetricsFilterAndSorting()
-print("Request serialized to JSON:\n{sep}\n{json}\n{sep}\n".format(
+logging.log(PRINT, "Request serialized to JSON:\n{sep}\n{json}\n{sep}\n".format(
     json=serialize_to_json(request),
     sep='-'*40,
 ))
@@ -35,7 +47,7 @@ print("Request serialized to JSON:\n{sep}\n{json}\n{sep}\n".format(
 t1 = time.time()
 select = api.getSelect(request)
 t2 = time.time()
-print("Got SELECT statement in ({time:.2f}ms):\n{sep}\n{select}\n{sep}\n".format(
+logging.log(PRINT, "Got SELECT statement in ({time:.2f}ms):\n{sep}\n{select}\n{sep}\n".format(
     time=(t2 - t1)*1000,
     select=select,
     sep='-'*40,
@@ -45,7 +57,7 @@ print("Got SELECT statement in ({time:.2f}ms):\n{sep}\n{select}\n{sep}\n".format
 t1 = time.time()
 data = api.getData(request)
 t2 = time.time()
-print("Got DATA ({time}ms):\n{sep}\n{data}\n{sep}\n".format(
+logging.log(PRINT, "Got DATA ({time:.2f}ms):\n{sep}\n{data}\n{sep}\n".format(
     time=(t2 - t1) * 1000,
     data=data,
     sep='-' * 40,
@@ -56,12 +68,12 @@ t1 = time.time()
 csv_filename = 'client_example_output.csv'
 api.GetCSV(request, csv_filename)
 t2 = time.time()
-print("DATA stored to file ({time:.2f}ms):\n{sep}\n{file}\n{sep}\n".format(
+logging.log(PRINT, "DATA stored to file ({time:.2f}ms):\n{sep}\n{file}\n{sep}\n".format(
     time=(t2 - t1) * 1000,
     file=csv_filename,
     sep='-' * 40,
 ))
 
-print("END")
+logging.log(PRINT, "END")
 
 
